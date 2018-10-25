fc7ebe68d9d139263604fe67c919c802a5b6f425
Huizhuo Gan <jokerandjoe@gmail.com>
2018-10-25 01:01:49 +0000

    add new variables

--- js/script.js
+++ js/script.js
@@ -4,13 +4,32 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
+var userChoice=$("#input").val();
+var computerChoice
+var winner
+var randomNumber=Math.random();
 
 
 
 // DOCUMENT READY FUNCTION BELOW
 
 $("#shoot").click(function() {
-    $("#userChoice").text($("#input").val());
-
+     randomNumber=Math.random();
+     userChoice=$("#input").val();
+    $("#userChoice").text(userChoice);
+    alert(randomNumber);
+   
+    if(randomNumber>0.6 && randomNumber<1) {
+         computerChoice="scissors" 
+    }
+   else if(randomNumber<0.6 && randomNumber>0.3){
+        computerChoice="paper" 
+    }
+    else if(randomNumber<0.3){
+         computerChoice="rock"
+    }
+    
+    $("#computerChoice").text(computerChoice);
 });
+
+
